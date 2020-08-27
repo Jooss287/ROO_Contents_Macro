6c070a5eacf3fba6b48070b53e4930400652d130
jooss287 <jooss287@gmail.com>
2020-08-27 16:59:40 +0900

    [fix] Algorithm change

---
+++ AppPlayer_autoClicker/Macro_AutoClick.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using Emgu.CV;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Windows.Media.Imaging;
+using System.IO;
+using System.Threading;
+using System.Windows.Threading;
+using System.Windows;
+
+using wPoint = System.Drawing.Point;
+
+namespace AppPlayer_autoClicker
+{
+    class Macro_AutoClick
+    {
+        #region dll import
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        internal static extern bool GetCursorPos(ref Win32Point pt);
+        [DllImport("user32.dll")]
+        static extern void mouse_event(int flag, int dx, int dy, int buttons, int extra);
+        [DllImport("user32.dll")]
+        static extern int SetCursorPos(int x, int y);
+
+        [StructLayout(LayoutKind.Sequential)]
+        internal struct Win32Point
+        {
+            public Int32 X;
+            public Int32 Y;
+        };
+        #endregion
+        readonly wPoint mouse_pos;
+        private wPoint capLT;
+        private wPoint capRB;
+        private Thread macroThread;
+        private bool ThreadLoop;
+        public Macro_AutoClick(wPoint param_mouse_pos, wPoint param_screenLT, wPoint param_screenRB)
+        {
+            mouse_pos = param_mouse_pos;
+            CalcTargetSize(param_screenLT, param_screenRB);
+            ThreadLoop = false;
+        }
+        
+            
+        private BitmapImage CaptureWindow(System.Drawing.Point lefttop, System.Drawing.Point rightbottom)
+        {
+            //int width = (int)SystemParameters.PrimaryScreenWidth;
+            //int height = (int)SystemParameters.PrimaryScreenHeight;
+            int width = rightbottom.X - lefttop.X;
+            int height = rightbottom.Y - lefttop.Y;
+
+            using (Bitmap bmp = new Bitmap(width, height, System.Drawing.Imaging.PixelFormat.Format32bppArgb))
+            {
+                using (Graphics gr = Graphics.FromImage(bmp))
+                {
+                    gr.CopyFromScreen(lefttop.X, lefttop.Y, 0, 0, bmp.Size);
+                }
+
+                using (MemoryStream memory = new MemoryStream())
+                {
+                    bmp.Save(memory, ImageFormat.Bmp);
+                    memory.Position = 0;
+                    BitmapImage bitmapimage = new BitmapImage();
+                    bitmapimage.BeginInit();
+                    bitmapimage.StreamSource = memory;
+                    bitmapimage.CacheOption = BitmapCacheOption.OnLoad;
+                    bitmapimage.EndInit();
+
+                    return bitmapimage;
+                }
+            }
+        }
+        public static System.Windows.Point GetMousePosition()
+        {
+            Win32Point w32Mouse = new Win32Point();
+            GetCursorPos(ref w32Mouse);
+            return new System.Windows.Point(w32Mouse.X, w32Mouse.Y);
+        }
+        private void MouseClick()
+        {
+            SetCursorPos(mouse_pos.X, mouse_pos.Y);
+            mouse_event((int)MouseFlag.ME_LEFTDOWN, mouse_pos.X, mouse_pos.Y, 0, 0);
+            mouse_event((int)MouseFlag.ME_LEFTUP, 0, 0, 0, 0);
+        }
+        private void CalcTargetSize(wPoint param_screenLT, wPoint param_screenRB)
+        {
+            const int template_width = 156;
+            const int template_height = 156;
+            const int template_screen_width = 1280;
+            const int template_screen_height = 720;
+            const int template_left_width = 78;
+            const int template_right_width = 78;
+            const int template_bottom_height = 25;
+            const int template_top_height = 131;
+
+            const double templateWidthRatio = (double)template_left_width / (double)template_screen_width;
+            const double templateTopRatio = (double)template_top_height / (double)template_screen_height;
+            const double templateBottomratio = (double)template_bottom_height / (double)template_screen_height;
+
+            int screen_width = param_screenRB.X - param_screenLT.X;
+            int screen_height = param_screenRB.Y - param_screenLT.Y;
+
+            double target_cap_width = templateWidthRatio * screen_width;
+            double target_cap_top = templateTopRatio * screen_height;
+            double target_cap_bottom = templateBottomratio * screen_height;
+
+            capLT = new wPoint(
+                mouse_pos.X - Convert.ToInt32(Math.Round(target_cap_width)), mouse_pos.Y - Convert.ToInt32(Math.Round(target_cap_top)));
+            capRB = new wPoint(
+                mouse_pos.X + Convert.ToInt32(Math.Round(target_cap_width)), mouse_pos.Y + Convert.ToInt32(Math.Round(target_cap_bottom)));
+        }
+        private Mat CaptureMatWindow()
+        {
+            BitmapImage bmpImage = CaptureWindow(capLT, capRB);
+            Mat matCapture = BitmapSourceExtension.ToMat(bmpImage);
+            matCapture.ConvertTo(matCapture, Emgu.CV.CvEnum.DepthType.Cv32F);
+
+            return matCapture;
+        }
+
+        public void StartThread()
+        {
+            if (ThreadLoop)
+                return ;
+            macroThread = new Thread(new ThreadStart(AutoFishing));
+            ThreadLoop = true;
+            macroThread.Start();
+        }
+        public void EndThread()
+        {
+            if (ThreadLoop)
+                ThreadLoop = false;
+        }
+        void AutoFishing()
+        {
+            while (ThreadLoop)
+            {
+                Thread.Sleep(200);
+                Mat refImg = CaptureMatWindow();
+                MouseClick();
+                
+                double similarity;
+                do
+                {
+                    if (!ThreadLoop)
+                    {
+                        Application.Current.Dispatcher.Invoke(DispatcherPriority.Normal, new Action(delegate
+                        {
+                            ((MainWindow)Application.Current.MainWindow).btn_start.Content = "매크로 시작";
+                        }));
+                        return;
+                    }
+                    similarity = CheckSimilarity(ref refImg);
+                    Thread.Sleep(50);
+                } while (similarity > -11);
+
+                MouseClick();
+                Thread.Sleep(2000);
+
+                MouseClick();
+                Thread.Sleep(200);
+            }
+            return;
+        }
+        private double CheckSimilarity(ref Mat refStateImg)
+        {
+            Mat tempImg = CaptureMatWindow();
+            
+            double compareRatio = CvInvoke.CompareHist(refStateImg, tempImg, Emgu.CV.CvEnum.HistogramCompMethod.Chisqr);
+
+            Application.Current.Dispatcher.Invoke(DispatcherPriority.Normal, new Action(delegate
+            {
+                ((MainWindow)Application.Current.MainWindow).img_reference.Source = BitmapSourceExtension.ToBitmapSource(tempImg);
+                ((MainWindow)Application.Current.MainWindow).btn_start.Content = Convert.ToString(Convert.ToInt32(compareRatio));
+            }));
+
+            return compareRatio;
+        }
+    }
+}
--- AppPlayer_autoClicker/MainWindow.xaml.cs
+++ AppPlayer_autoClicker/MainWindow.xaml.cs
@@ -3,15 +3,11 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
 using System.Windows.Media.Imaging;
-using System.Drawing;
 using System.Runtime.InteropServices;
-using System.Threading;
 using System.Windows.Threading;
 using System.Text.RegularExpressions;
 
-using Emgu.CV;
-using System.IO;
-using System.Drawing.Imaging;
+using wPoint = System.Drawing.Point;
 
 namespace AppPlayer_autoClicker
 {
@@ -26,22 +22,8 @@ namespace AppPlayer_autoClicker
     public partial class MainWindow : Window
     {
         readonly BitmapImage targetImage;
-        readonly BitmapImage targetImage1;
-        ProcessInfobyMousePositionExtension ext = new ProcessInfobyMousePositionExtension();
-        const int template_width = 156;
-        const int template_height = 156;
-        const int template_screen_width = 1280;
-        const int template_screen_height = 720;
-        const int template_left_width = 78;
-        const int template_right_width = 78;
-        const int template_bottom_height = 25;
-        const int template_top_height = 131;
-
         public bool threadloop = false;
-
-        System.Drawing.Point mouse_pos;
-        System.Drawing.Point lefttop;
-        System.Drawing.Point rightbottom;
+        Macro_AutoClick mac;
 
         private void Label_MouseDoubleClick(object sender, MouseButtonEventArgs e)
         {
@@ -55,10 +37,7 @@ namespace AppPlayer_autoClicker
             mWindow.Title = "Roo 자동 낚시 (Ver.200825.01)";
             InitializeUserInterface();
 
-            string filename = Environment.CurrentDirectory + "\\Resource\\targetImg.png";
-            targetImage = new BitmapImage(new Uri(filename, UriKind.Relative));
-            targetImage1 = new BitmapImage(new Uri("Resource/targetImg.png", UriKind.Relative));
-            img_reference.Source = targetImage1;
+            img_reference.Source = new BitmapImage(new Uri("Resource/targetImg.png", UriKind.Relative));
             
             Mouse.Capture(this);
             DispatcherTimer mousePosTimer = new DispatcherTimer();
@@ -77,205 +56,36 @@ namespace AppPlayer_autoClicker
             txt_mouse_click_posY.Text = "693";
         }
         
-        public static System.Windows.Point GetMousePosition()
-        {
-            Win32Point w32Mouse = new Win32Point();
-            GetCursorPos(ref w32Mouse);
-            return new System.Windows.Point(w32Mouse.X, w32Mouse.Y);
-        }
+        
 
+        #region CallBack
         private void MousePosCallBack(object sender, EventArgs e)
         {
-            System.Windows.Point pointToWindow = GetMousePosition();
+            Point pointToWindow = Macro_AutoClick.GetMousePosition();
             txt_mouse_pos.Text = pointToWindow.X.ToString() + ", " + pointToWindow.Y.ToString();
         }
-        private void CallBack_SelProcess(object  sender, EventArgs e)
-        {
-            System.Windows.Point pointToWindow = GetMousePosition();
-            ext.SetPositon(Convert.ToInt32(pointToWindow.X), Convert.ToInt32(pointToWindow.Y));
-            ext.GetFocusRect();
-        }
-        
         private void Btn_start_Click(object sender, RoutedEventArgs e)
         {
             if (threadloop == false)
             {
-                mouse_pos.X = System.Convert.ToInt32(txt_mouse_click_posX.Text);
-                mouse_pos.Y = System.Convert.ToInt32(txt_mouse_click_posY.Text);
-                lefttop = new System.Drawing.Point(Convert.ToInt32(screen_lefttop_x.Text), Convert.ToInt32(screen_lefttop_y.Text));
-                rightbottom = new System.Drawing.Point(Convert.ToInt32(screen_rightbottom_x.Text), Convert.ToInt32(screen_rightbottom_y.Text));
+                wPoint mouse_pos = new wPoint(
+                    Convert.ToInt32(txt_mouse_click_posX.Text),
+                    Convert.ToInt32(txt_mouse_click_posY.Text));
+                wPoint lefttop = new wPoint(Convert.ToInt32(screen_lefttop_x.Text), Convert.ToInt32(screen_lefttop_y.Text));
+                wPoint rightbottom = new wPoint(Convert.ToInt32(screen_rightbottom_x.Text), Convert.ToInt32(screen_rightbottom_y.Text));
 
-                Thread t1 = new Thread(new ThreadStart(AutoFishing));
+                mac = new Macro_AutoClick(mouse_pos, lefttop, rightbottom) ;
+                mac.StartThread();
                 threadloop = true;
-                SetHook();
-                t1.Start();
             }
             else
-            { 
+            {
+                mac.EndThread();
                 threadloop = false;
             }
-
             return;
         }
-
-        void AutoFishing()
-        {
-            while(threadloop)
-            {
-                double default_sim = 0.0;
-                for (int i = 0; i < 5; i++)
-                {
-                    default_sim += CheckSimilarity();
-                    Thread.Sleep(100);
-                }
-                default_sim = default_sim / 5;
-
-
-                SetCursorPos(mouse_pos.X, mouse_pos.Y);
-                mouse_event((int)MouseFlag.ME_LEFTDOWN, mouse_pos.X, mouse_pos.Y, 0, 0);
-                mouse_event((int)MouseFlag.ME_LEFTUP, 0, 0, 0, 0);
-
-                double similarity;
-                do
-                {
-                    if (!threadloop)
-                    {
-                        UnHook();
-                        Dispatcher.Invoke(DispatcherPriority.Normal, new Action(delegate
-                        {
-                            btn_start.Content = "매크로 종료";
-                        }));
-                        return;
-                    }
-                    similarity = CheckSimilarity();
-                    Thread.Sleep(100);
-                } while (similarity > default_sim*0.8);
-
-                SetCursorPos(mouse_pos.X, mouse_pos.Y);
-                mouse_event((int)MouseFlag.ME_LEFTDOWN, mouse_pos.X, mouse_pos.Y, 0, 0);
-                mouse_event((int)MouseFlag.ME_LEFTUP, 0, 0, 0, 0);
-
-                Thread.Sleep(2000);
-
-                SetCursorPos(mouse_pos.X, mouse_pos.Y);
-                mouse_event((int)MouseFlag.ME_LEFTDOWN, mouse_pos.X, mouse_pos.Y, 0, 0);
-                mouse_event((int)MouseFlag.ME_LEFTUP, 0, 0, 0, 0);
-
-                Thread.Sleep(200);
-            }
-
-            UnHook();
-            return;
-        }
-
-        private BitmapImage CaptureWindow(System.Drawing.Point lefttop, System.Drawing.Point rightbottom)
-        {
-            //int width = (int)SystemParameters.PrimaryScreenWidth;
-            //int height = (int)SystemParameters.PrimaryScreenHeight;
-            int width = rightbottom.X - lefttop.X;
-            int height = rightbottom.Y - lefttop.Y;
-
-            using (Bitmap bmp = new Bitmap(width, height, System.Drawing.Imaging.PixelFormat.Format32bppArgb))
-            {
-                using (Graphics gr = Graphics.FromImage(bmp))
-                {
-                    gr.CopyFromScreen(lefttop.X, lefttop.Y, 0, 0, bmp.Size);
-                }
-
-                using (MemoryStream memory = new MemoryStream())
-                {
-                    bmp.Save(memory, ImageFormat.Bmp);
-                    memory.Position = 0;
-                    BitmapImage bitmapimage = new BitmapImage();
-                    bitmapimage.BeginInit();
-                    bitmapimage.StreamSource = memory;
-                    bitmapimage.CacheOption = BitmapCacheOption.OnLoad;
-                    bitmapimage.EndInit();
-
-                    return bitmapimage;
-                }
-            }
-        }
-
-        private double CheckSimilarity()
-        {
-            
-            int screen_width = rightbottom.X - lefttop.X;
-            int screen_height = rightbottom.Y - lefttop.Y;
-
-            double templateWidthRatio = ((double)template_left_width) / (double)template_screen_width * (double)screen_width;
-            double templateTopRatio = ((double)template_top_height) / (double)template_screen_height * (double)screen_height;
-            double templateBottomratio = ((double)template_bottom_height) / (double)template_screen_height * (double)screen_height;
-
-            
-            System.Drawing.Point template_lefttop = new System.Drawing.Point(
-                mouse_pos.X - Convert.ToInt32(Math.Round(templateWidthRatio)), mouse_pos.Y - Convert.ToInt32(Math.Round(templateTopRatio)));
-            System.Drawing.Point template_rightbottom = new System.Drawing.Point(
-                mouse_pos.X + Convert.ToInt32(Math.Round(templateWidthRatio)), mouse_pos.Y + Convert.ToInt32(Math.Round(templateBottomratio)));
-
-            BitmapImage bmpImage = CaptureWindow(template_lefttop, template_rightbottom);
-            Mat matCapture = BitmapSourceExtension.ToMat(bmpImage);
-
-            //BitmapImage temp = targetImage;
-            Mat targetResize = new Mat();
-            Dispatcher.Invoke(DispatcherPriority.Normal, new Action(delegate
-            {
-                targetResize = BitmapSourceExtension.ToMat(targetImage);
-            }));
-             
-            CvInvoke.Resize(targetResize, targetResize, new System.Drawing.Size(template_rightbottom.X - template_lefttop.X, template_rightbottom.Y - template_lefttop.Y), 0, 0, Emgu.CV.CvEnum.Inter.Linear);
-
-            matCapture.ConvertTo(matCapture, Emgu.CV.CvEnum.DepthType.Cv32F);
-            targetResize.ConvertTo(targetResize, Emgu.CV.CvEnum.DepthType.Cv32F);
-            
-            //for ( int i = 0; i < matCapture.Rows; i++)
-            //{
-            //    for ( int j = 0; j < matCapture.Cols; j++)
-            //    {
-            //        MatExtension.SetValue(matCapture, i, j, 0);
-            //        MatExtension.SetValue(targetResize, i, j, 0);
-            //    }
-            //}
-
-
-            double compareRatio = CvInvoke.CompareHist(targetResize, matCapture, Emgu.CV.CvEnum.HistogramCompMethod.Chisqr);
-
-            Dispatcher.Invoke(DispatcherPriority.Normal, new Action(delegate
-            {
-                img_reference.Source = BitmapSourceExtension.ToBitmapSource(matCapture);
-                btn_start.Content = Convert.ToString(Convert.ToInt32(compareRatio));
-            }));
-
-            return compareRatio;
-            //CompareHist
-        }
-
-
-        #region dll import
-        [DllImport("user32.dll")]
-        [return: MarshalAs(UnmanagedType.Bool)]
-        internal static extern bool GetCursorPos(ref Win32Point pt);
-        [DllImport("user32.dll")]
-        static extern void mouse_event(int flag, int dx, int dy, int buttons, int extra);
-        [DllImport("user32.dll")]
-        static extern int SetCursorPos(int x, int y);
-
-        [StructLayout(LayoutKind.Sequential)]
-        internal struct Win32Point
-        {
-            public Int32 X;
-            public Int32 Y;
-        };
         #endregion
-
-        private void btn_sel_process_Click(object sender, RoutedEventArgs e)
-        {
-            Mouse.Capture(this);
-            DispatcherTimer mousePosTimer = new DispatcherTimer();
-            mousePosTimer.Interval = TimeSpan.FromSeconds(0.05);
-            mousePosTimer.Tick += new EventHandler(CallBack_SelProcess);
-            mousePosTimer.Start();
-        }
 
         #region keyboard hook
         [DllImport("user32.dll")]
@@ -331,8 +141,7 @@ namespace AppPlayer_autoClicker
         }
         #endregion
 
-        
-
+        #region UI Setting
         public bool IsNumeric(string source)
         {
             Regex regex = new Regex("[^0-9.-]+");
@@ -354,6 +163,7 @@ namespace AppPlayer_autoClicker
                 )
             );
         }
+        #endregion
     }
 
 
